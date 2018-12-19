0fdb410f6590b9bf62ad3dde347417a75f56112a
vadim <shelbyua17@gmail.com>
2018-12-19 23:03:01 +0200

    first commit

---
+++ api/src/app/Controllers/Http/UserController.js
@@ -0,0 +1,25 @@
+'use strict'
+
+class UserController {
+	async index() {
+		return { status: 'sadasdasd'}
+	}
+
+	async store() {
+
+	}
+
+	async show() {
+
+	}
+
+	async update() {
+
+	}
+
+	async destroy() {
+
+	}
+}
+
+module.exports = UserController
