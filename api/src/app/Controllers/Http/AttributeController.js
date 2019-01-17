a38dc871a795841d8b4ec186136bccf0f03f87c5
vadim <shelbyua17@gmail.com>
2019-01-17 16:43:12 +0200

    fix: start controllers

--- api/src/app/Controllers/Http/AttributeController.js
+++ api/src/app/Controllers/Http/AttributeController.js
@@ -1,6 +1,53 @@
+const ProductType = use('App/Models/Type');
+
 class AttributeController {
-  async index() {
-    return {};
+  async index({ params }) {
+    const productType = await ProductType.findOrFail(params.id);
+    const attrs = productType.attributes().fetch();
+    return attrs;
+  }
+
+  async store({ params, request }) {
+    const { attribute } = request.all();
+    const productType = await ProductType.findOrFail(params.id);
+    const newAttr = productType.attributes().create({
+      attribute
+    });
+    return newAttr;
+  }
+
+  async update({ params, request }) {
+    const { attribute } = request.all();
+    const productType = await ProductType.findOrFail(params.id);
+    await productType
+      .attributes()
+      .where('id', params.attrid)
+      .update({
+        attribute
+      });
+    const updatedAttr = await productType
+      .attributes()
+      .where('id', params.attrid)
+      .first();
+    return updatedAttr;
+  }
+
+  async show({ params }) {
+    const productType = await ProductType.findOrFail(params.id);
+    const attr = await productType
+      .attributes()
+      .where('id', params.attrid)
+      .first();
+    return attr;
+  }
+
+  async destroy({ params, response }) {
+    const productType = await ProductType.findOrFail(params.id);
+    await productType
+      .attributes()
+      .where('id', params.attrid)
+      .delete();
+    return response.json({ msg: 'Ok' });
   }
 }
 
